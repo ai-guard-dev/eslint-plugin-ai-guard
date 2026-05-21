57d57589c73ec27e2b72b630870f79afcd789f74
Yash Jadhav <yashjadhav2102@gmail.com>
2026-05-21 19:33:27 +0530

    fix: use generic eslint-disable-next-line to avoid rule not found errors

--- tests/helpers/rule-tester.ts
+++ tests/helpers/rule-tester.ts
@@ -26,7 +26,7 @@ export const ruleTester = new RuleTester({
  */
 let tsRuleTesterInstance: RuleTester;
 try {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  // eslint-disable-next-line
   const tsParser = require('@typescript-eslint/parser') as object;
   tsRuleTesterInstance = new RuleTester({
     languageOptions: {
